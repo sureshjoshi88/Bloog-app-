5491fa6977c4ee062a6612e695ba4f32fad6d95a
sureshjoshi88 <sj3795789@gmail.com>
2025-11-16 21:02:53 +0530

    add new file

---
+++ src/component/Default.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Default = () => {
+  return (
+    <div className='h-100'>
+      <p className='flex justify-center items-center text-2xl font-medium'>No Page found</p>
+    </div>
+  )
+}
+
+export default Default
