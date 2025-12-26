eb76935ac27045b54b55bd50c243ba73fc39df94
sureshjoshi88 <sj3795789@gmail.com>
2025-12-26 18:15:36 +0530

    updates

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from './context/themeReducer.jsx'
-  
+import {Provider} from "react-redux"
+import { store } from './redux/store/store.js'
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
   <ThemeProvider>
   <StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </StrictMode>
   </ThemeProvider> 
   </BrowserRouter>,
